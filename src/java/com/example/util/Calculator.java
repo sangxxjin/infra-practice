32ec72abd319fb652b6699c4b130134cbf6d129c
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-04-08 11:22:28 +0900

    test

---
+++ src/java/com/example/util/Calculator.java
@@ -0,0 +1,21 @@
+package com.example.util;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int divide(int a, int b) {
+		return a / b; // 코드리뷰 b가 0일때 에러
+	}
+
+	public int multiply(int a, int b) {
+		if (a == 0 || b == 0) return 0; // 코드리뷰 필요없는 조건
+		return a * b;
+	}
+}
